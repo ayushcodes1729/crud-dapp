681e21772adbc2d0866a9ae8e99e98da9e3e3852
ayushcodes1729 <ayushtiwari0803@gmail.com>
2025-06-22 03:17:17 +0530

    Init app

---
+++ anchor/programs/crud-app/src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
+
+#[program]
+pub mod counter {
+    use super::*;
+}
+
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub message: String
+}
